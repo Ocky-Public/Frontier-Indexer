f53ce82c7a52e1a5b68de28bc5eb04c00b72c32f
Ocky <ocky@algonet.xyz>
2026-04-11 10:53:05 +0200

    Implemented models for energy config.

---
+++ src/models/world/primitives/energy/energy_config.rs
@@ -0,0 +1,37 @@
+use diesel::prelude::*;
+use serde::Deserialize;
+
+use sui_indexer_alt_framework::FieldCount;
+use sui_types::dynamic_field::Field;
+use sui_types::object::Object;
+
+use crate::schema::indexer::energy_config;
+
+#[derive(Deserialize)]
+pub struct MoveField {}
+
+#[derive(Insertable, Debug, Clone, FieldCount)]
+#[diesel(table_name = energy_config)]
+pub struct StoredEnergyConfig {
+    assembly_id: String,
+    energy_cost: i64,
+    entry_object_id: String,
+    checkpoint_updated: i64,
+}
+
+impl StoredEnergyConfig {
+    pub fn from_object(obj: &Object, checkpoint_updated: i64) -> Self {
+        let move_obj = obj.data.try_as_move().expect("Object is not a Move object");
+        let bytes = move_obj.contents();
+
+        let entry: Field<u64, u64> =
+            bcs::from_bytes(bytes).expect("Failed to deserialze Entry object");
+
+        Self {
+            assembly_id: entry.name.to_string(),
+            energy_cost: entry.value as i64,
+            entry_object_id: obj.id().to_string(),
+            checkpoint_updated,
+        }
+    }
+}
