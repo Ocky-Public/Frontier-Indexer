5f2195a27f52cc96bafd4445c667e8e4af1c0335
Ocky <ocky@algonet.xyz>
2026-04-11 14:05:11 +0200

    Added package_id to the energy config table, ensuring uniqueness incase other structs are also indexed.

--- src/models/world/primitives/energy/energy_config.rs
+++ src/models/world/primitives/energy/energy_config.rs
@@ -13,6 +13,7 @@ pub struct MoveField {}
 #[derive(Insertable, Debug, Clone, FieldCount)]
 #[diesel(table_name = energy_config)]
 pub struct StoredEnergyConfig {
+    package_id: String,
     assembly_id: String,
     energy_cost: i64,
     entry_object_id: String,
@@ -20,7 +21,7 @@ pub struct StoredEnergyConfig {
 }
 
 impl StoredEnergyConfig {
-    pub fn from_object(obj: &Object, checkpoint_updated: i64) -> Self {
+    pub fn from_object(obj: &Object, owner_pkg_id: String, checkpoint_updated: i64) -> Self {
         let move_obj = obj.data.try_as_move().expect("Object is not a Move object");
         let bytes = move_obj.contents();
 
@@ -28,6 +29,7 @@ impl StoredEnergyConfig {
             bcs::from_bytes(bytes).expect("Failed to deserialze Entry object");
 
         Self {
+            package_id: owner_pkg_id,
             assembly_id: entry.name.to_string(),
             energy_cost: entry.value as i64,
             entry_object_id: obj.id().to_string(),
