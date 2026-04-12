509213a801ca4816b65ab3696264e7dd2d1290df
Ocky <ocky@algonet.xyz>
2026-04-12 22:16:17 +0200

    Implemented handler for fuel burning updated events.

--- src/handlers/world/primitives/fuel.rs
+++ src/handlers/world/primitives/fuel.rs
@@ -1,5 +1,6 @@
 pub mod fuel_burning_started_handler;
 pub mod fuel_burning_stopped_handler;
+pub mod fuel_burning_updated_handler;
 pub mod fuel_config_handler;
 pub mod fuel_efficiency_removed_handler;
 pub mod fuel_efficiency_set_handler;
