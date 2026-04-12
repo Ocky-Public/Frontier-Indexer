9a5db620c300a67e340012c12a06d81db0fff553
Ocky <ocky@algonet.xyz>
2026-04-12 10:19:29 +0200

    Implemented models for energy released events.

---
+++ src/models/world/primitives/energy/event_energy_released.rs
@@ -0,0 +1,49 @@
+use chrono::{DateTime, Utc};
+use diesel::prelude::*;
+use serde::Deserialize;
+
+use sui_indexer_alt_framework::FieldCount;
+use sui_sdk_types::Address;
+use sui_types::event::Event;
+
+use crate::handlers::EventMeta;
+use crate::schema::indexer::events_energy_released;
+
+#[derive(Deserialize)]
+pub struct MoveEnergyReleased {
+    energy_source_id: Address,
+    assembly_type_id: u64,
+    energy_released: u64,
+    total_reserved_energy: u64,
+}
+
+#[derive(Insertable, Debug, Clone, FieldCount)]
+#[diesel(table_name = events_energy_released)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct StoredEnergyRelease {
+    event_id: String,
+    occurred_at: DateTime<Utc>,
+    id: String,
+    type_id: i64,
+    released: i64,
+    reserved_total: i64,
+}
+
+impl StoredEnergyRelease {
+    pub fn from_event(event: &Event, meta: &EventMeta) -> Self {
+        let move_event: MoveEnergyReleased =
+            bcs::from_bytes(&event.contents).expect("Failed to deserialize Energy Released event");
+
+        let occurred_at = DateTime::from_timestamp_millis(meta.checkpoint_timestamp_ms())
+            .expect("Failed to parse checkpoint timestamp into DateTime");
+
+        Self {
+            event_id: meta.event_digest(),
+            occurred_at,
+            id: move_event.energy_source_id.to_hex(),
+            type_id: move_event.assembly_type_id as i64,
+            released: move_event.energy_released as i64,
+            reserved_total: move_event.total_reserved_energy as i64,
+        }
+    }
+}
