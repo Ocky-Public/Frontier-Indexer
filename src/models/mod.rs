5a2a07bd44b16407e441a487cd19eaa70be283aa
Ocky <ocky@algonet.xyz>
2026-04-13 13:45:29 +0200

    Added type name struct for extensions.

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,10 @@
+use serde::{Deserialize, Serialize};
+
 pub mod app;
 pub mod system;
 pub mod world;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct MoveTypeName {
+    pub name: String,
+}
