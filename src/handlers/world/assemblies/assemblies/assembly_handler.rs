b485db2684dc0a114924abd6113ec21841820f94
Ocky <ocky@algonet.xyz>
2026-04-13 22:39:49 +0200

    Refactored assemblies to use new context.

--- src/handlers/world/assemblies/assemblies/assembly_handler.rs
+++ src/handlers/world/assemblies/assemblies/assembly_handler.rs
@@ -1,8 +1,7 @@
 use async_trait::async_trait;
-use move_core_types::account_address::AccountAddress;
 use std::collections::hash_map::Entry;
-use std::collections::{HashMap, HashSet};
-use std::str::FromStr;
+use std::collections::HashMap;
+
 use std::sync::Arc;
 
 use diesel::prelude::*;
@@ -20,53 +19,23 @@ use sui_indexer_alt_framework::pipeline::Processor;
 use sui_indexer_alt_framework::postgres::{Connection, Db};
 use sui_indexer_alt_framework::types::full_checkpoint_content::Checkpoint;
 
-use crate::handlers::is_indexed_tx;
 use crate::models::world::StoredAssembly;
 
 use crate::AppContext;
 
 pub struct AssemblyHandler {
     ctx: AppContext,
-    package_set: HashSet<AccountAddress>,
 }
 
 impl AssemblyHandler {
     pub fn new(ctx: &AppContext) -> Self {
-        let package_set: HashSet<AccountAddress> = ctx
-            .get_world_package_strings()
-            .iter()
-            .filter_map(|s| AccountAddress::from_str(s).ok())
-            .collect();
-
-        Self {
-            ctx: ctx.clone(),
-            package_set,
-        }
+        Self { ctx: ctx.clone() }
     }
 
     fn is_assembly(&self, obj: &Object) -> bool {
         let module_name = "assembly";
         let struct_name = "Assembly";
-
-        if let Some(move_type) = obj.type_() {
-            if let Some(tag) = move_type.other() {
-                if !self.package_set.contains(&tag.address) {
-                    return false;
-                }
-
-                if tag.module.as_str() != module_name {
-                    return false;
-                }
-
-                if tag.name.as_str() != struct_name {
-                    return false;
-                }
-
-                return true;
-            }
-        }
-
-        false
+        self.ctx.is_world_object(obj, module_name, struct_name)
     }
 }
 #[derive(FieldCount)]
@@ -85,7 +54,7 @@ impl Processor for AssemblyHandler {
         let cp_sequence = checkpoint.summary.sequence_number as i64;
 
         for tx in &checkpoint.transactions {
-            if !is_indexed_tx(tx, &checkpoint.object_set, &self.ctx) {
+            if !self.ctx.is_indexed_tx(tx, &checkpoint.object_set) {
                 continue;
             }
 
