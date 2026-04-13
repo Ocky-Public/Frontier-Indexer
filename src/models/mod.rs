f187762f4addaaedc03617643a7b44de6ae1f8d7
Ocky <ocky@algonet.xyz>
2026-04-13 19:28:48 +0200

    Implemented function to break TypeNames into its components.

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,4 +1,7 @@
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+use move_core_types::language_storage::StructTag;
 
 pub mod app;
 pub mod system;
@@ -7,4 +10,22 @@ pub mod world;
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MoveTypeName {
     pub name: String,
+}
+
+impl MoveTypeName {
+    pub fn to_components(&self) -> (String, String, String) {
+        let type_name = if self.name.starts_with("0x") {
+            self.name.clone()
+        } else {
+            format!("0x{}", &self.name)
+        };
+
+        let tag = StructTag::from_str(&type_name).expect("Could not parse TypeName into StructTag");
+
+        let package_id = tag.address.to_canonical_string(true);
+        let module_name = tag.module.to_string();
+        let struct_name = tag.name.to_string();
+
+        (package_id, module_name, struct_name)
+    }
 }
