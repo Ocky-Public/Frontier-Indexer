df090aee6b655008cb1bb361251a7817cd610b5d
Ocky <ocky@algonet.xyz>
2026-04-15 23:15:50 +0200

    Housekeeping.

--- src/handlers/world.rs
+++ src/handlers/world.rs
@@ -20,9 +20,9 @@ pub use assemblies::gates::gate_extension_revoked_handler::*;
 pub use assemblies::gates::gate_handler::*;
 pub use assemblies::gates::gate_jumped_handler::*;
 pub use assemblies::gates::gate_linked_handler::*;
-pub use assemblies::gates::gate_unlinked_handler::*;
 pub use assemblies::gates::gate_permit_handler::*;
 pub use assemblies::gates::gate_permit_issued_handler::*;
+pub use assemblies::gates::gate_unlinked_handler::*;
 
 pub use assemblies::network_nodes::network_node_created_handler::*;
 pub use assemblies::network_nodes::network_node_handler::*;
