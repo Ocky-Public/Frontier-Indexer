80b45c87bb2fd5513c81d9cc047de91663292dbc
Ocky <ocky@algonet.xyz>
2026-04-15 10:47:20 +0200

    Added pipelines for storage units.

--- src/main.rs
+++ src/main.rs
@@ -254,6 +254,12 @@ async fn main() -> Result<(), anyhow::Error> {
                 indexer.sequential_pipeline(world::NetworkNodeCreatedHandler::new(&context), sequential.clone()).await?;
                 indexer.sequential_pipeline(world::NetworkNodeHandler::new(&context), sequential.clone()).await?;
 
+                // Storage Units
+                indexer.sequential_pipeline(world::StorageUnitCreatedHandler::new(&context), sequential.clone()).await?;
+                indexer.sequential_pipeline(world::StorageUnitExtensionAuthorizedHandler::new(&context), sequential.clone()).await?;
+                indexer.sequential_pipeline(world::StorageUnitExtensionRevokedHandler::new(&context), sequential.clone()).await?;
+                indexer.sequential_pipeline(world::StorageUnitHandler::new(&context), sequential.clone()).await?;
+
                 // Turrets
                 indexer.sequential_pipeline(world::TurretCreatedHandler::new(&context), sequential.clone()).await?;
                 indexer.sequential_pipeline(world::TurretExtensionAuthorizedHandler::new(&context), sequential.clone()).await?;
