5804437b39a77d3f0be0757fdd4c7d26ae069af6
Ocky <ocky@algonet.xyz>
2026-04-19 10:22:11 +0200

    Reworked vector to hash set for better performance.

--- src/handlers/world/assemblies/gates/gate_config_handler.rs
+++ src/handlers/world/assemblies/gates/gate_config_handler.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use move_core_types::account_address::AccountAddress;
 use std::collections::hash_map::Entry;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
 use std::sync::Arc;
 
@@ -207,8 +207,8 @@ impl Handler for GateConfigHandler {
     ) -> anyhow::Result<usize> {
         use crate::schema::indexer::gate_config::dsl::*;
 
-        let mut upsert_map: HashMap<String, &StoredGateConfig> = HashMap::new();
-        let mut to_delete = Vec::new();
+        let mut to_upsert: HashMap<String, &StoredGateConfig> = HashMap::new();
+        let mut to_delete: HashSet<String> = HashSet::new();
 
         for action in batch {
             match action {
@@ -216,7 +216,7 @@ impl Handler for GateConfigHandler {
                     self.ctx.tables.add_table(conn, table).await?;
                 }
                 GateConfigAction::Upsert(config) => {
-                    let current = upsert_map.entry(config.type_id.to_string());
+                    let current = to_upsert.entry(config.type_id.to_string());
 
                     match current {
                         Entry::Occupied(mut entry) => {
@@ -229,14 +229,16 @@ impl Handler for GateConfigHandler {
                         }
                     }
                 }
-                GateConfigAction::Delete(id_str) => to_delete.push(id_str.clone()),
+                GateConfigAction::Delete(id_str) => {
+                    to_delete.insert(id_str.clone());
+                }
             }
         }
 
         // Remove any updates for which deletions exist.
-        upsert_map.retain(|obj_id, _| !to_delete.contains(obj_id));
+        to_upsert.retain(|obj_id, _| !to_delete.contains(obj_id));
 
-        let final_values: Vec<&StoredGateConfig> = upsert_map.into_values().collect();
+        let final_values: Vec<&StoredGateConfig> = to_upsert.into_values().collect();
 
         if !final_values.is_empty() {
             diesel::insert_into(gate_config)
