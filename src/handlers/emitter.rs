56d9859bcac2e2689ec8cc208cc06878a7759e7e
Ocky <ocky@algonet.xyz>
2026-04-27 08:46:30 +0000

    Added dispatchabled trait to the emitter with implementations to handle common collection types.

--- src/handlers/emitter.rs
+++ src/handlers/emitter.rs
@@ -1,8 +1,34 @@
 use futures::future::join_all;
 use serde::Serialize;
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use crate::transports::Transport;
+
+pub trait Dispatchable<I> {
+    fn dispatch_iter(&self) -> Box<dyn Iterator<Item = &I> + Send + '_>;
+    fn is_empty(&self) -> bool;
+}
+
+impl<I: Send + Sync> Dispatchable<I> for Vec<I> {
+    fn dispatch_iter(&self) -> Box<dyn Iterator<Item = &I> + Send + '_> {
+        Box::new(self.iter())
+    }
+
+    fn is_empty(&self) -> bool {
+        self.is_empty()
+    }
+}
+
+impl<I: Send + Sync> Dispatchable<I> for HashMap<String, I> {
+    fn dispatch_iter(&self) -> Box<dyn Iterator<Item = &I> + Send + '_> {
+        Box::new(self.values())
+    }
+
+    fn is_empty(&self) -> bool {
+        self.is_empty()
+    }
+}
 
 pub struct Emitter<I: Serialize + Send + Sync + 'static> {
     transports: Vec<Arc<dyn Transport<I>>>,
@@ -13,7 +39,25 @@ impl<I: Serialize + Send + Sync + 'static> Emitter<I> {
         Self { transports }
     }
 
-    pub async fn dispatch(&self, pipeline: &'static str, item: &I) {
+    pub fn dispatch<B>(self: &Arc<Self>, pipeline: &'static str, batch: &B)
+    where
+        B: Dispatchable<I> + Clone + Send + 'static,
+    {
+        if batch.is_empty() {
+            return;
+        }
+
+        let batch = batch.clone();
+        let emitter = Arc::clone(self);
+
+        tokio::spawn(async move {
+            for entry in batch.dispatch_iter() {
+                emitter.dispatch_entry(pipeline, entry).await;
+            }
+        });
+    }
+
+    async fn dispatch_entry(&self, pipeline: &'static str, item: &I) {
         if self.transports.is_empty() {
             return;
         }
