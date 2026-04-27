138592ed189b45c0c91cae911e43bc1fc8847719
Ocky <ocky@algonet.xyz>
2026-04-27 17:41:51 +0000

    Removed reference to specific schema.

--- migrations/00000000000000_diesel_initial_setup/up.sql
+++ migrations/00000000000000_diesel_initial_setup/up.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION diesel_manage_updated_at(_tbl regclass) RETURNS VOID AS $$
 BEGIN
-    EXECUTE format('CREATE TRIGGER set_updated_at BEFORE UPDATE ON indexer.%s
+    EXECUTE format('CREATE TRIGGER set_updated_at BEFORE UPDATE ON %s
                     FOR EACH ROW EXECUTE PROCEDURE diesel_set_updated_at()', _tbl);
 END;
 $$ LANGUAGE plpgsql;
