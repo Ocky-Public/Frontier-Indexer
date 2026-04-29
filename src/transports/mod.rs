6beffd0f070562266e23af41e23026dc7b1e385b
Ocky <ocky@algonet.xyz>
2026-04-29 09:12:04 +0000

    Implemented Transport routing that enforces each pipeline to be handled with a defult no-op

--- src/transports/mod.rs
+++ src/transports/mod.rs
@@ -1,11 +1,88 @@
 use async_trait::async_trait;
 use serde::Serialize;
 
+use crate::handlers::world::*;
+use crate::models::world::*;
+
+pub mod amqp;
+pub mod nats;
+pub mod redis;
+pub mod socketio;
+
 #[async_trait]
-pub trait Transport<I>: Send + Sync + 'static
+pub trait Routing<I>: Send + Sync + 'static
 where
     I: Serialize + Send + Sync,
 {
+    async fn send(&self, _pipeline: &'static str, _item: &I) -> anyhow::Result<()> {
+        Ok(())
+    }
+}
+
+mod private {
+    use super::*;
+
+    pub trait Router: Send + Sync + 'static {}
+
+    impl<T> Router for T where
+        T: super::Routing<StoredOwnerCapCreated>
+            + super::Routing<OwnerCapAction>
+            + super::Routing<StoredOwnerCapTransferred>
+            + super::Routing<StoredAssemblyCreated>
+            + super::Routing<AssemblyAction>
+            + super::Routing<StoredExtensionFrozen>
+            + super::Routing<GateConfigAction>
+            + super::Routing<StoredGateCreated>
+            + super::Routing<StoredGateExtensionAuthorized>
+            + super::Routing<StoredGateExtensionRevoked>
+            + super::Routing<GateAction>
+            + super::Routing<StoredGateJumped>
+            + super::Routing<StoredGateLinked>
+            + super::Routing<GatePermitAction>
+            + super::Routing<StoredGatePermitIssued>
+            + super::Routing<StoredGateUnlinked>
+            + super::Routing<StoredNetworkNodeCreated>
+            + super::Routing<NetworkNodeAction>
+            + super::Routing<StoredStorageUnitCreated>
+            + super::Routing<StoredStorageUnitExtensionAuthorized>
+            + super::Routing<StoredStorageUnitExtensionRevoked>
+            + super::Routing<StorageUnitAction>
+            + super::Routing<StoredTurretCreated>
+            + super::Routing<StoredTurretExtensionAuthorized>
+            + super::Routing<StoredTurretExtensionRevoked>
+            + super::Routing<TurretAction>
+            + super::Routing<StoredCharacterCreated>
+            + super::Routing<CharacterAction>
+            + super::Routing<StoredKillmail>
+            + super::Routing<EnergyConfigAction>
+            + super::Routing<StoredEnergyProductionStarted>
+            + super::Routing<StoredEnergyProductionStopped>
+            + super::Routing<StoredEnergyReleased>
+            + super::Routing<StoredEnergyReserved>
+            + super::Routing<StoredFuelBurningStarted>
+            + super::Routing<StoredFuelBurningStopped>
+            + super::Routing<StoredFuelBurningUpdated>
+            + super::Routing<FuelConfigAction>
+            + super::Routing<StoredFuelDeleted>
+            + super::Routing<StoredFuelDeposited>
+            + super::Routing<StoredFuelEfficiencyRemoved>
+            + super::Routing<StoredFuelEfficiencySet>
+            + super::Routing<StoredFuelWithdrawn>
+            + super::Routing<InventoryAction>
+            + super::Routing<StoredItemBurned>
+            + super::Routing<StoredItemDeposited>
+            + super::Routing<StoredItemDestroyed>
+            + super::Routing<ItemAction>
+            + super::Routing<StoredItemMinted>
+            + super::Routing<StoredItemWithdrawn>
+            + super::Routing<StoredLocationRevealed>
+            + super::Routing<StoredStatusChanged>
+    {
+    }
+}
+
+#[async_trait]
+pub trait Transport<I>: private::Router {
     fn id(&self) -> String;
 
     async fn send(&self, pipeline: &'static str, item: &I) -> anyhow::Result<()>;
