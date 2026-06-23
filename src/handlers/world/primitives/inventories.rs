a434ce0ebe40e1c7e42003e86c4142d63545be0e
Ocky <ocky@algonet.xyz>
2026-06-23 14:48:54 +0000

    Incorporated new and handlers.

--- src/handlers/world/primitives/inventories.rs
+++ src/handlers/world/primitives/inventories.rs
@@ -1,7 +1,9 @@
 pub mod inventory_handler;
 pub mod item_burned_handler;
 pub mod item_deposited_handler;
+pub mod item_deposited_v2_handler;
 pub mod item_destroyed_handler;
 pub mod item_handler;
 pub mod item_minted_handler;
 pub mod item_withdrawn_handler;
+pub mod item_withdrawn_v2_handler;
---
+++ src/handlers/world/rifts.rs
@@ -0,0 +1,3 @@
+pub mod rift_handler;
+pub mod rift_location_broadcasted_handler;
+pub mod rift_spawned_handler;
